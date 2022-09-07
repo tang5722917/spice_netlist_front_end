2caf1523bbeb9bd7f60aec3f8c5a71482931ebc8
Donald duck <tang5722917@163.com>
2022-09-07 10:55:31 +0800

    Add some design

--- SNFE.py
+++ SNFE.py
@@ -2,7 +2,7 @@
 Author: Donald duck tang5722917@163.com
 Date: 2022-09-05 15:54:09
 LastEditors: Donald duck tang5722917@163.com
-LastEditTime: 2022-09-05 18:14:47
+LastEditTime: 2022-09-07 10:49:09
 FilePath: \spice_netlist_front_end\SNFE.py
 Description: Spice Netlist Front End
              Startup python 
@@ -18,7 +18,8 @@ import logging
 
 sys.path.append("./src")
 import SNFE_help
-
+import netlist_check
+import netlist_deal_main
 
 SNFE_argv = sys.argv
 
@@ -43,6 +44,13 @@ else :
             para_list.append(SNFE_argv[i-1][1])
         else:
             circuit_file_name = SNFE_argv[i-1]
+            if Debug_enable =='1':
+                logging.info('Input circuit netlist name: ' + circuit_file_name  )
+            
     for i in range(0,len(para_list)):
         if(para_list[i] == 'h') :
             SNFE_help.print_help()
+        elif(para_list[i] == 'help') :
+            SNFE_help.print_help()
+        if Debug_enable =='1':
+            logging.info('The run parameter option: \'' + para_list[i] + '\''  )
--- src/SNFE_help.py
+++ src/SNFE_help.py
@@ -2,11 +2,16 @@
 Author: Donald duck tang5722917@163.com
 Date: 2022-09-05 18:10:49
 LastEditors: Donald duck tang5722917@163.com
-LastEditTime: 2022-09-05 18:12:03
+LastEditTime: 2022-09-07 10:46:41
 FilePath: \spice_netlist_front_end\src\SNFE_help.py
 Description:  SNFE help file
 
 Copyright (c) 2022 by Donald duck tang5722917@163.com, All Rights Reserved. 
 '''
 def print_help() :
-    print("S(pice) N(etlist) F(ront) E(end) help")
+    print("*******************************************************************************\n")
+    print("S(pice) N(etlist) F(ront) E(end) help info")
+    print("SNFE.py [option] files")
+    print("files  -- Circuit netlist file .sp/.SP/.cir/.CIR")
+    print("[option] :\n -h/--help : show the help info for SNFE")
+    print("\n*******************************************************************************")
